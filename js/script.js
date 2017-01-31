b45c4dc6031212ef543a108623b82c265b5f2b59
Carla Heywood <awesome1@carlas-mbp.dynamic.ucsd.edu>
2017-01-30 20:40:42 -0800

    Update
    Animate menu button
    Add menu options
    Attempt to fix mobile centering

--- js/script.js
+++ js/script.js
@@ -17,11 +17,16 @@ $(window).scroll(function () {
 		'transform' : 'translate(0px ,  ' + wScroll / 8 + '%)'
 	});
 });
-
+// Burger menu animated
+function myFunction(x) {
+    "use strict";
+    x.classList.toggle("change");
+}
 // Smooth Scrolling
-$(document).ready(function(){
+$(document).ready(function () {
   // Add smooth scrolling to all links
-  $("a").on('click', function(event) {
+    "use strict";
+    $("a").on('click', function (event) {
 
     // Make sure this.hash has a value before overriding default behavior
     if (this.hash !== "") {
