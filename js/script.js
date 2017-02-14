87ac5691821d32ab6cb9a2344d3b36447150a5f8
Carla Heywood <awesomeimac@Carlas-iMac.local>
2017-02-14 15:29:36 -0800

    Update
    Mobile Update

--- js/script.js
+++ js/script.js
@@ -18,10 +18,26 @@ $(window).scroll(function () {
 	});
 });
 // Burger menu animated
-function myFunction(x) {
+function animateMenu(x) {
     "use strict";
     x.classList.toggle("change");
 }
+// Side Menu animated
+var acc = document.getElementsByClassName("dropNav");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].onclick = function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight){
+  	  panel.style.maxHeight = null;
+    } else {
+  	  panel.style.maxHeight = panel.scrollHeight + "px";
+    } 
+  }
+}
+//-----------------
 // Smooth Scrolling
 $(document).ready(function () {
   // Add smooth scrolling to all links
@@ -29,22 +45,22 @@ $(document).ready(function () {
     $("a").on('click', function (event) {
 
     // Make sure this.hash has a value before overriding default behavior
-    if (this.hash !== "") {
+        if (this.hash !== "") {
       // Prevent default anchor click behavior
-      event.preventDefault();
+            event.preventDefault();
 
       // Store hash
-      var hash = this.hash;
+            var hash = this.hash;
 
       // Using jQuery's animate() method to add smooth page scroll
       // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
-      $('html, body').animate({
-        scrollTop: $(hash).offset().top
-      }, 800, function(){
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 800, function () {
    
         // Add hash (#) to URL when done scrolling (default click behavior)
-        window.location.hash = hash;
-      });
-    } // End if
-  });
+                window.location.hash = hash;
+            });
+        } // End if
+    });
 });
